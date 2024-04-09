a6315b12457c2a54036c882f2ab0497590ef2e5d
hoanghai912 <haih2212@gmail.com>
2024-04-09 10:34:03 +0700

    Update

--- App.js
+++ App.js
@@ -25,20 +25,21 @@ import {View, Text, Image, ScrollView, TextInput} from 'react-native';
 export default function App() {
   return (
     <ScrollView>
-    <Text className="text-center">Some text</Text>
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text>Some more text</Text>
-      <Image
-        source={{
-          uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-        }}
-        style={{width: 200, height: 200}}
-      />
-    </View>
-    <TextInput
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      defaultValue="You can type in me"
-    />
-  </ScrollView>
+      
+      <View className="flex-1 justify-center items-center bg-white">
+        <Text className="text-center">Some text Now</Text>
+        <Text>Some more text</Text>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{width: 200, height: 200}}
+        />
+        <TextInput
+          className="text-center w-1/2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          defaultValue="You can type in me"
+        />
+      </View>
+    </ScrollView>
   );
 }
---
+++ README.md
@@ -0,0 +1,17 @@
+### Prepare the environment
+- Run this code in cmd
+``` 
+npm install -g expo-cli
+```
+
+### After clone the project
+- Run this code in cmd in directory of project
+``` 
+npm install 
+```
+
+### Run the project
+- Run this code in cmd in directory of project
+``` 
+npx expo start --tunnel
+```
